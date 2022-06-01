24ca623e5a968b32c7508c9f32e0f70061ceaf18
Sarvang Dave <sarvang00@gmail.com>
2022-06-01 23:01:14 +0530

    Added gif display @ "/"

--- ch1/5-server/server3.go
+++ ch1/5-server/server3.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 )
 
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", lissajous)
+	http.HandleFunc("/header-handler", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -25,4 +38,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
+}
+
+func lissajous(w http.ResponseWriter, r *http.Request) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	w.Header().Set("Content-Type", "image/gif")
+	w.Header().Set("Cache-Control", "no-cache, no-store, no-transform")
+	gif.EncodeAll(w, &anim) // NOTE: ignoring encoding errors
 }
