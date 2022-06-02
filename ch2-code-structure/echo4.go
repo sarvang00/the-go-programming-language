431ae02e2cae63719e5417f587c97b1c1367fb03
Sarvang Dave <sarvang00@gmail.com>
2022-06-02 05:53:48 +0000

    Another echo. Uses flag and string packages

---
+++ ch2-code-structure/echo4.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// A flag to omit newline
+var n = flag.Bool("n", false, "omit trailing newline")
+
+// A seperator flag; default to " "
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	// Format the string as per the value of seperator flag
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	// Add a newline if flag n is not added
+	if !*n {
+		fmt.Println()
+	}
+}
