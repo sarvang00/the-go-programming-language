29c5b14d3492376618623a5ce0a8d8c4d6499e43
Sarvang Dave <sarvang00@gmail.com>
2022-06-03 10:30:12 +0000

    ex.3.11. Enhance comma to deal with float numbers

--- ch3-basic-data-types/comma.go
+++ ch3-basic-data-types/comma.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // comma inserts commas in a non-negative decimal integer string.
@@ -18,5 +19,12 @@ func comma(s string) string {
 func main() {
 	str := bufio.NewReader(os.Stdin)
 	ip_str, _ := str.ReadString('\n')
-	fmt.Println(comma(ip_str))
+	ip_str = strings.TrimSpace(ip_str)
+	var rem string = ""
+	idx := strings.Index(ip_str, ".")
+	if idx != -1 {
+		rem = ip_str[idx:]
+		ip_str = ip_str[:idx]
+	}
+	fmt.Println(comma(ip_str) + rem)
 }
